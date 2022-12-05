d47d1ab463d873d5c49b32d6e1d7aa7019281ca4
Michael Jelly <53475252+michaeljelly@users.noreply.github.com>
2022-12-05 23:36:45 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version:5.7
 /**
  This file is part of the Reductio package.
  (c) Sergio Fernández <fdz.sergio@gmail.com>
@@ -10,8 +10,31 @@
 import PackageDescription
 
 let package = Package(
-  name: "Reductio",
-     version: "1.0.0"
-
+    name: "Reductio",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "Reductio",
+            targets: ["Reductio"],
+             version: "1.0.0"
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(
+            name: "Reductio",
+            dependencies: []
+        ),
+        .testTarget(
+            name: "ReductioTests",
+            dependencies: ["Reductio"]
+        ),
+    ],
+    swiftLanguageVersions: [.v5]
+    version: "1.0.0"
 )
-
