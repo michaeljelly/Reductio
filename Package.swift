0b7b8db88d9d44257d0290049b2b8d800bd30188
Michael Jelly <53475252+michaeljelly@users.noreply.github.com>
2022-12-05 23:43:52 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -15,8 +15,7 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Reductio",
-            targets: ["Reductio"],
-             version: "1.0.1"
+            targets: ["Reductio"]
         ),
     ],
     dependencies: [
